28f0cdced1441b82ad94026727e730576da67731
michalfirszt <michalfiszu@gmail.com>
2021-06-26 21:24:41 +0200

    Load events

--- src/store/events/index.ts
+++ src/store/events/index.ts
@@ -1,33 +1,64 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+import { loadStatuses } from '../../constants';
 
 type Event = {
   id: number;
   name: string;
+  slug: string;
+  latitude: number;
+  longitude: number;
   description: string;
+  active: boolean;
 };
 
 type State = {
-  events: Event[];
+  events: { [key: number]: Event };
+  effects: {
+    loadEventListEffect: {
+      status: string;
+      error: null | string;
+    };
+  };
 };
 
 const initialState: State = {
-  events: [],
+  events: {},
+  effects: {
+    loadEventListEffect: {
+      status: loadStatuses.NOT_LOADING,
+      error: null,
+    },
+  },
 };
+
+export const loadEventList = createAsyncThunk(
+  'events/loadEventList',
+  async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/api/event/list`
+    );
+
+    return response.data;
+  }
+);
 
 const eventSlice = createSlice({
   name: 'events',
   initialState,
-  reducers: {
-    loadEventList: (state) => {
-      axios
-        .get(`${process.env.REACT_APP_API_URL}/api/events`)
-        .then((response) => {
-          state.events = response.data;
-        });
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loadEventList.fulfilled, (state, action) => {
+      state.events = action.payload.reduce(
+        (list: { [key: number]: Event }, event: Event) => (
+          (list[event.id] = event), list
+        ),
+        {}
+      );
+      state.effects.loadEventListEffect.status = loadStatuses.LOAD_SUCCESS;
+    });
   },
 });
 
-export const { loadEventList } = eventSlice.actions;
 export default eventSlice.reducer;
